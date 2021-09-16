e658d3679928d9616803b30c5e5bdb2ec1cc992a
Stefan Niedermann <info@niedermann.it>
2021-09-16 18:27:20 +0200

    Unify tags and categories page

--- archive.php
+++ archive.php
@@ -1,14 +1,12 @@
 <?php
 /**
- * Template Name: Kategorie-Seiten
- * Auflistung von Artikeln in einer bestimmten Kategorie.
+ * Template Name: Archiv-Seiten
+ * Auflistung von Artikeln in einer bestimmten Kategorie oder mit einem bestimmten Tag.
  */
 
 /* Set News to current Menu Item */
 add_filter( 'nav_menu_css_class', 'get_news_nav_class', 10, 2 );
 
-// Single Style
-wp_enqueue_style('category', get_template_directory_uri().'/category.css' );
 wp_enqueue_style('teaser', get_template_directory_uri().'/css/teaser.css' );
 
 get_header();
@@ -19,7 +17,13 @@ get_header();
 			<span><a href="/news">News</a></span>
 		</li>
 		<li>
-			<span>Kategorien</span>
+			<span><?php
+			if(is_tag()) {
+				echo 'Tags';
+			} elseif(is_category()) {
+				echo 'Kategorien';
+			}
+			?></span>
 		</li>
 		<li>
 			<span><?php echo single_cat_title("", false); ?></span>
