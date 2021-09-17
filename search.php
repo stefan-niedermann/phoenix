a7feee8cb7c0124023465b011da7521218db4b1b
Stefan Niedermann <info@niedermann.it>
2021-09-17 10:23:18 +0200

    Enhance search results

--- search.php
+++ search.php
@@ -8,6 +8,22 @@ wp_enqueue_style('tags', get_template_directory_uri() . '/css/tags.css');
 wp_enqueue_style('pagination', get_template_directory_uri() . '/css/pagination.css');
 
 get_header();
+
+if(empty(get_search_query()) || empty($wp_query->found_posts)) {
+?>
+<main class="no-results container center">
+	<form action="?">
+		<input type="search" name="s" placeholder="Suchbegriff eingeben">
+		<button type="submit" class="btn-floating btn-large waves-effect waves-light red-4"><i class="material-icons">search</i></button>
+	</form>
+	<?php
+	if(empty($wp_query->found_posts)) {
+		echo '<p>Hierfür haben wir leider keine Ergebnisse.</p>';
+	}
+	?>
+</main>
+<?php
+} else {
 ?>
 <header class="search-header section grey lighten-3">
 	<div class="container center">
@@ -28,4 +44,5 @@ get_header();
 	<?php phoenix_pagination($wp_query->max_num_pages); ?>
 	</section>
 </main>
+<?php } ?>
 <?php get_footer(); ?>
