a59080728a198a6990db054e33ad75a5366c094c
Stefan Niedermann <info@niedermann.it>
2021-09-17 15:12:06 +0200

    Enhanced author page

--- author.php
+++ author.php
@@ -10,28 +10,35 @@ wp_enqueue_style('cards', get_template_directory_uri() . '/css/cards.css');
 wp_enqueue_style('tags', get_template_directory_uri() . '/css/tags.css');
 get_header();
 ?>
-<main class="container flow-text">
-	<h1><?php the_author_meta('display_name'); ?></h1>
-	<p><?php the_author_meta('description'); ?></p>
-	<?php if (!empty(get_the_author_meta("user_url"))) { ?>
-		<h2>Website</h2>
-		<p><a href="<?php the_author_meta('user_url'); ?>"><?php the_author_meta('user_url'); ?></a></p>
-	<?php } ?>
-	<h2>Letzte Artikel</h2>
-	<?php
-	$teaserquery = new WP_Query(array(
-		'posts_per_page' => 6,
-		'post_type' => 'post',
-		'post_status' => 'publish',
-		'author' => get_the_author_meta("id")
-	));
-	echo '<div class="teaser-row">';
-	while ($teaserquery->have_posts()) {
-		$teaserquery->the_post();
-		the_teaser_entry(array('col', 'l6'));
-	}
-	wp_reset_postdata();
-	echo "</div>";
-	?>
+<main class="section">
+	<div class="flow-text container">
+		<h1><?php the_author_meta('display_name'); ?></h1>
+		<p><?php the_author_meta('description'); ?></p>
+		<?php if (!empty(get_the_author_meta("user_url"))) { ?>
+			<h2>Website</h2>
+			<ul class="author-meta">
+				<li><a href="<?php the_author_meta('user_url'); ?>"><i class="material-icons">public</i><?php the_author_meta('user_url'); ?></a></li>
+			</ul>
+		<?php } ?>
+	</div>
 </main>
+<section class="section grey lighten-4">
+	<div class="container">
+		<div class="teaser-row">
+		<?php
+			$teaserquery = new WP_Query(array(
+				'posts_per_page' => 6,
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'author' => get_the_author_meta("id")
+			));
+			while ($teaserquery->have_posts()) {
+				$teaserquery->the_post();
+				the_teaser_entry(array('col', 'l6'));
+			}
+			wp_reset_postdata();
+		?>
+		</div>
+	</div>
+</section>
 <?php get_footer(); ?>
