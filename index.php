b7adf1849990d2e13171042da51daec49031a22f
Stefan Niedermann <info@niedermann.it>
2021-09-17 14:23:40 +0200

    Add teaser to solo pages

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
  * Template Name: Fallback
  * Generisches Template für eine Seite, der kein Template zugewiesen ist
  */
+wp_enqueue_style('index', get_template_directory_uri() . '/index.css');
+
+$currentPost = get_post();
+$customValues = get_post_custom_values("Post Teaser Kategorie", $currentPost->ID);
+
+if(!empty($customValues) && count($customValues) > 0) {
+	wp_enqueue_style('cards', get_template_directory_uri() . '/css/cards.css');
+	wp_enqueue_style('tags', get_template_directory_uri() . '/css/tags.css');
+}
+
 get_header(); ?>
 <main class="container flow-text">
 <?php if ( have_posts() ) { ?>
@@ -29,73 +39,30 @@ get_header(); ?>
 }
 ?>
 </main>
-<?php /*
-$currentPost = get_post();
-$customValues = get_post_custom_values("Post Teaser Kategorie", $currentPost->ID);
-if(count($customValues) > 0) {
-	echo '<div style="display: none;">';
-	$teaserquery = new WP_Query(array(
-		'posts_per_page' => 2,
-		'post_type' => 'post',
-		'post_status' => 'publish',
-		'cat' => $customValues[0]
-	));
-	while ( $teaserquery->have_posts() ) {
-		$teaserquery->the_post();
-		?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<header>
-					<h1>
-						<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-					</h1>
-					<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail("medium_large"); ?></a>
-				</header>
-				<div>
-					<?php the_excerpt(); ?>
+<?php
+	if(!empty($customValues) && count($customValues) > 0) {
+		$category = get_category_by_slug($customValues[0]);
+		if($category) { ?>
+			<section class="section grey lighten-4 trivia">
+				<div class="container teaser-row">
+				<?php
+					$teaserquery = new WP_Query(array(
+						'posts_per_page' => 4,
+						'post_type' => 'post',
+						'post_status' => 'publish',
+						'cat' => $category->term_id
+					));
+					while ( $teaserquery->have_posts() ) {
+						$teaserquery->the_post();
+						the_teaser_entry(array('col', 'l6'));
+					}
+					wp_reset_postdata();
+				?>
+				<a href="<?php get_category_link($category) ?>" class="btn btn-large waves-effect waves-light">Weitere Artikel</a>
 				</div>
-				<footer>
-					<?php
-						// Translators: used between list items, there is a space after the comma.
-						$categories_list = get_the_category_list( __( ', ', 'twentytwelve' ) );
-
-						// Translators: used between list items, there is a space after the comma.
-						$tag_list = get_the_tag_list( '', __( ', ', 'twentytwelve' ) );
-
-						$date = sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a>',
-							esc_url( get_permalink() ),
-							esc_attr( get_the_time() ),
-							esc_attr( get_the_date( 'c' ) ),
-							esc_html( get_the_date() )
-						);
-
-						$author = sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span>',
-							esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-							esc_attr( sprintf( __( 'View all posts by %s', 'twentytwelve' ), get_the_author() ) ),
-							get_the_author()
-						);
-
-						// Translators: 1 is category, 2 is tag, 3 is the date and 4 is the author's name.
-						if ( $tag_list ) {
-							$utility_text = __( 'Dieser Beitrag wurde in %1$s am %3$s<span class="by-author"> von %4$s</span> veröffentlicht. Schlagworte: %2$s', 'twentytwelve' );
-						} elseif ( $categories_list ) {
-							$utility_text = __( 'Dieser Beitrag wurde in %1$s am %3$s<span class="by-author"> von %4$s</span> veröffentlicht.', 'twentytwelve' );
-						} else {
-							$utility_text = __( 'Dieser Beitrag wurde in %3$s<span class="by-author"> von %4$s</span> veröffentlicht.', 'twentytwelve' );
-						}
-
-						printf(
-							$utility_text,
-							$categories_list,
-							$tag_list,
-							$date,
-							$author
-						);
-					?>
-				</footer>
-			</article>
-		<?php
+			</section>
+			<?php
+		}
 	}
-	echo '</div>';
-}*/
-get_footer();
+	get_footer();
 ?>
