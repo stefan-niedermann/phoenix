7e03ed966a84ae4423a550bf6f81cef609063d07
Stefan Niedermann <info@niedermann.it>
2021-09-27 20:10:45 +0200

    Fix build

--- build.js
+++ build.js
@@ -1,14 +1,12 @@
 const fs = require('fs-extra');
 const execSync = require('child_process').execSync;
 
-// Build
-
-prepareWorkspace('dist');
-copyAllSources('src', 'dist');
-copyNecessaryDependencies('dist');
-translate('dist/languages');
-
-// Helper functions
+const build = () => {
+    prepareWorkspace('dist');
+    copyAllSources('src', 'dist');
+    copyNecessaryDependencies('dist');
+    translate('dist/languages');
+}
 
 const prepareWorkspace = (dist) => {
     if (fs.existsSync(dist)) {
@@ -19,7 +17,7 @@ const prepareWorkspace = (dist) => {
 
 const copyAllSources = (src, dist) => fs.copySync(src, dist);
 
-const copyNecessaryDependencies = () => {
+const copyNecessaryDependencies = (dist) => {
     fs.copySync('node_modules/@materializecss/materialize/dist/css/materialize.min.css', `${dist}/css/materialize.min.css`);
     fs.copySync('node_modules/@materializecss/materialize/dist/js/materialize.min.js', `${dist}/js/materialize.min.js`);
 }
@@ -29,3 +27,5 @@ const translate = (i18n) =>
         .filter(file => file.endsWith('.po'))
         .map(file => file.substring(0, file.length - 3))
         .forEach(file => execSync(`msgfmt ${i18n}/${file}.po -o ${i18n}/${file}.mo`));
+
+build();
