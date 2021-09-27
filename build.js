cfeb682213f987323837bb720bfb9ca685d8c017
Stefan Niedermann <info@niedermann.it>
2021-09-27 20:01:03 +0200

    Translate all available languages

--- build.js
+++ build.js
@@ -3,14 +3,27 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 const build = async () => {
+    // Clean up workspace
     if (fs.existsSync('dist')) {
         fs.rmSync('dist', { recursive: true });
     }
     fs.mkdirSync('dist');
+
+    // Copy all sources
     fs.copySync('src', 'dist');
+
+    // Copy necessary dependencies
     fs.copySync('node_modules/@materializecss/materialize/dist/css/materialize.min.css', 'dist/css/materialize.min.css');
     fs.copySync('node_modules/@materializecss/materialize/dist/js/materialize.min.js', 'dist/js/materialize.min.js');
-    ['de_DE'].forEach(async (lang) => await exec(`msgfmt dist/languages/${lang}.po -o dist/languages/${lang}.mo`));
+
+    // Translate i18n files
+    const i18n = 'dist/languages';
+    await Promise.all(
+        fs.readdirSync(i18n)
+            .filter(file => file.endsWith('.po'))
+            .map(file => file.substring(0, file.length - 3))
+            .map(file => exec(`msgfmt ${i18n}/${file}.po -o ${i18n}/${file}.mo`))
+    );
 }
 
 build();
