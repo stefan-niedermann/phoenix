43193bc576951e453ceb69ff7ab5812eb8790548
Stefan Niedermann <info@niedermann.it>
2021-10-08 18:45:21 +0200

    Extend e2e tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,12 +1,27 @@
 describe('The Home Page', () => {
-    it('successfully loads', () => {
+    it('should only contain links that can be loaded successfully', () => {
         cy.visit('/', {
-            qs: {'donotcachepage': Cypress.env('WP_SUPER_CACHE_BUSTER')}
+            qs: { 'donotcachepage': Cypress.env('WP_SUPER_CACHE_BUSTER') }
         });
 
         cy.contains('Feuerwehr Aurachhöhe');
         cy.contains('Unsere Freizeit für Ihre Sicherheit');
 
-        cy.contains('Es gab einen kritischen Fehler auf deiner Website.').should('not.exist');
+        const visitedPages = new Set('/');
+
+        const checkLinkedUrlsAreValid = (page, visitedPages) => {
+            visitedPages.add(page);
+            cy.get('a:not([href*="#"], [href|="mailto:"], [href|="tel:"], [href*="wp-content"])').each($anchor => {
+                const url = $anchor.attr('href');
+                if (!visitedPages.has(url)) {
+                    visitedPages.add(url);
+                    cy.request(url, {
+                        qs: { 'donotcachepage': Cypress.env('WP_SUPER_CACHE_BUSTER') }
+                    });
+                }
+            });
+        }
+
+        checkLinkedUrlsAreValid('/', visitedPages);
     })
 });
