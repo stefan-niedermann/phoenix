a2dcf4a9a0622f58e54704b16682a94fba2c3157
Stefan Niedermann <info@niedermann.it>
2021-10-08 10:16:55 +0200

    Rename cypress env var

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,7 @@
 describe('The Home Page', () => {
     it('successfully loads', () => {
         cy.visit('/', {
-            qs: {'donotcachepage': Cypress.env('CYPRESS_CACHE_BUSTER')}
+            qs: {'donotcachepage': Cypress.env('WP_SUPER_CACHE_BUSTER')}
         });
 
         cy.contains('Feuerwehr Aurachhöhe');
