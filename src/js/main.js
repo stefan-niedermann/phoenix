3cda1e3535d5a47acb3c3efa913cd2ee2a272355
Stefan Niedermann <info@niedermann.it>
2021-10-11 08:26:32 +0200

    #2 Disable context menu for all images

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,31 @@ try {
 } catch (e) {
     console.error(e);
 }
+
+try {
+    const disableContextMenu = (elem) => {
+        elem.addEventListener('contextmenu', (e) => e.preventDefault(), false);
+    }
+    document.addEventListener('DOMContentLoaded', () => {
+        Array
+            .from(document.getElementsByTagName('img'))
+            .forEach(img => disableContextMenu(img));
+
+        new MutationObserver((mutations) => {
+            mutations
+                .filter(mutation => mutation.type === 'childList')
+                .forEach((mutation) => {
+                    Array.from(mutation.addedNodes)
+                        .filter(node => node instanceof HTMLImageElement)
+                        .forEach(node => disableContextMenu(node));
+                });
+        }).observe(document.body, {
+            attributes: false,
+            subtree: true,
+            childList: true,
+            characterData: false
+        });
+    }, false);
+} catch (e) {
+    console.error(e);
+}
--- src/single.js
+++ src/single.js
@@ -1,13 +1,3 @@
-try {
-	document.addEventListener('DOMContentLoaded', () => {
-		// Prevent Context Menu Click
-		Array.from(document.querySelectorAll('main img'))
-			.forEach(img => img.addEventListener('contextmenu', (e) => e.preventDefault(), false));
-	}, false);
-} catch (e) {
-	console.error(e);
-}
-
 if (window.speechSynthesis) {
 	try {
 		const voice = window.speechSynthesis
